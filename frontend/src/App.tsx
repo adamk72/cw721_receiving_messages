fafed690eebcb6b214f6a0b0c11609741ce8055a
Adam Kecskes <adam.kecskes@philabs.xyz>
2022-12-12 18:56:27 -0600

    feat: display token info; replaces contract info

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Component, createSignal, onMount } from 'solid-js';
+import { Component, createSignal, For, onMount, Show } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
@@ -9,14 +9,29 @@ import { MalagaInfo } from "../MalagaInfo"
 const App: Component = () => {
   onMount(async () => {
     const client = await CosmWasmClient.connect(MalagaInfo.rpc)
-    setContract(await client.getContracts(3216))
+    const id = (await client.getContracts(3216)).toString()
+
+    let tokensA;
+    try {
+      const {tokens} = await client.queryContractSmart(id, { "tokens": { "owner": "wasm1kmhmzgw7wvt9saaq8z2pk5aajxsznpz7c4l5m0" } })
+      tokensA = tokens
+    } catch (error) {
+      setError((error as Error).message)
+    }
+    setTokens(tokensA)
   })
-  const [contractId, setContract] = createSignal();
+  const [tokens, setTokens] = createSignal();
+  const [error, setError] = createSignal();
 
   return (
     <div class={styles.App}>
-      <main>
-        <pre>Contract: {contractId() as string || "awaiting..."}</pre>
+      <main >
+        <pre>Contract: {<For each={tokens() as string[]}>{(token, i) =>
+          <li>
+            {i() + 1}: {token}
+          </li>
+        }</For>}</pre>
+        <Show when={error()}><pre>Error: {error() as string} </pre></Show>
       </main>
     </div>
   );
