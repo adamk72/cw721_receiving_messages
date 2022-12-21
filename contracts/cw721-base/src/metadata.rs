0c386c12a5044127e93213b7be2b42bcc4c2466c
Adam Kecskes <adam.kecskes@philabs.xyz>
2022-12-21 13:25:11 -0600

    chore: add metadata structs

--- contracts/cw721-base/src/metadata.rs
+++ contracts/cw721-base/src/metadata.rs
@@ -1,36 +1,44 @@
-use cosmwasm_std::Addr;
-use serde::{Deserialize, Serialize};
-use universe::species::SapienceScale;
+use cosmwasm_schema::cw_serde;
+// use cosmwasm_std::Addr;
+// use universe::species::SapienceScale;
 
-#[derive(Serialize, Deserialize)]
-pub struct TokenMetadata {
-    pub title: Option<String>,
-    pub description: Option<String>,
-    pub media: Option<String>, // URL to (preferably to decentralized) media.
+// pub struct TokenMetadata {
+//     pub title: Option<String>,
+//     pub description: Option<String>,
+//     pub media: Option<String>, // URL to (preferably to decentralized) media.
 
-    // from NEAR standard; will see if all are desired/needed
-    pub issued_at: Option<String>, // ISO 8601 datetime when token was issued or minted
-    pub expires_at: Option<String>, // ISO 8601 datetime when token expires
-    pub starts_at: Option<String>, // ISO 8601 datetime when token starts being valid
-    pub updated_at: Option<String>, // ISO 8601 datetime when token was last updated
+//     // from NEAR standard; will see if all are desired/needed
+//     pub issued_at: Option<String>, // ISO 8601 datetime when token was issued or minted
+//     pub expires_at: Option<String>, // ISO 8601 datetime when token expires
+//     pub starts_at: Option<String>, // ISO 8601 datetime when token starts being valid
+//     pub updated_at: Option<String>, // ISO 8601 datetime when token was last updated
 
-    pub dna: Option<String>,
-    pub species: Option<String>,
-    pub sapience: Option<SapienceScale>,
-    pub origin: Option<Addr>,   // Planet address
-    pub identity: Option<Addr>, // The owner's wallet address
-}
+//     pub dna: Option<String>,
+//     pub species: Option<String>,
+//     pub sapience: Option<SapienceScale>,
+//     pub origin: Option<Addr>,   // Planet address
+//     pub identity: Option<Addr>, // The owner's wallet address
+// }
 
 /* For later usage, maybe */
-pub struct Attribute {
-    trait_type: String,
-    value: String,
+#[cw_serde]
+pub struct Trait {
+    pub display_type: Option<String>,
+    pub trait_type: String,
+    pub value: String,
 }
-/// Following: https://docs.opensea.io/docs/metadata-standards
+
+// see: https://docs.opensea.io/docs/metadata-standards
+#[cw_serde]
+#[derive(Default)]
 pub struct OpenMetadata {
-    pub description: Option<String>, // A human readable description of the item. Markdown is supported.
-    pub external_url: Option<String>, // This is the URL that will appear below the asset's image on OpenSea and will allow users to leave OpenSea and view the item on your site.
-    pub name: Option<String>,         // Name of the item.
-    pub image: Option<String>, // This is the URL to the image of the item. Can be just about any type of image (including SVGs, which will be cached into PNGs by OpenSea), and can be IPFS URLs or paths. We recommend using a 350 x 350 image.
-    pub attributes: Vec<Attribute>,
+    pub image: Option<String>,
+    pub image_data: Option<String>,
+    pub external_url: Option<String>,
+    pub description: Option<String>,
+    pub name: Option<String>,
+    pub attributes: Option<Vec<Trait>>,
+    pub background_color: Option<String>,
+    pub animation_url: Option<String>,
+    pub youtube_url: Option<String>,
 }
